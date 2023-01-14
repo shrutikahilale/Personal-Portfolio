6f578ffcf82253e8fcfa1682d8692c9c2a0dbf24
shrutikahilale <shrutikahilale8@gmail.com>
2023-01-14 20:39:44 +0530

    Carousel implementation for project section in js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// NAV-LINKS
+
 var currentLink = 0;
 // changeBg(currentLink);
 
@@ -14,4 +16,30 @@ function changeBg(idx) {
   nav_links[currentLink].style.backgroundColor = "#fff";
   nav_links[currentLink].style.color = "#9875FB";
   nav_links[currentLink].style.fontWeight = "700";
+}
+
+// CAROUSEL
+var currentSlide = 0;
+
+function changeSlide(incr) {
+  currentSlide += incr;
+  slideShow(currentSlide);
+}
+
+function slideShow(idx) {
+  var slides = document.getElementsByClassName("carousel-item");
+
+  if (idx >= slides.length) {
+    currentSlide = 0;
+  } else if (idx < 0) {
+    currentSlide = slides.length - 1;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[currentSlide].style.display = "flex";
+  slides[currentSlide].style.flexdirection = "column";  
+  slides[currentSlide].style.gap = "16px"; 
 }
